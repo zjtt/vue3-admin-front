455d974dd0dd7c48ff1f2554f0d398c8f7cad406
zjtt <chinaso@chinasodeMacBook-Air.local>
2023-03-03 15:56:35 +0800

    feat: 首页布局

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import router from "@/router/index"
 import { createPinia } from "pinia"
 import ElementPlus from "element-plus" // 这里我们直接全量引入
 import "element-plus/dist/index.css"
+// 初始化css，重置css默认样式,保持各个浏览器统一
+import "normalize.css/normalize.css"
+// 全局css
+import "@/styles/index.scss"
 
 const app = createApp(App)
 app.use(ElementPlus)
